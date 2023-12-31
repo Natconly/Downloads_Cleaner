0ede3abe5a61546ec535c8a2c46756269df3da93
Natconly <79179892+Natconly@users.noreply.github.com>
2023-12-31 17:58:45 -0600

    Create a loop to interate through Downloads list

--- cleaner.py
+++ cleaner.py
@@ -4,10 +4,14 @@ import os
 # Need to have the code access the downloads folder
 path = os.chdir('../Downloads')
 downloads_list = os.listdir(path)
-print(downloads_list)
+# print(downloads_list)
 
-#Loop through each file and determine a "type", ie photo, pdf, misc
-
+# Loop through each file and determine a "type", ie photo, pdf, misc
+for file in downloads_list:
+    if os.path.isfile(file):
+        print(file)
 
 
 #Once identified, move file
+
+#os.path.exists() - verifys that a file/directory location actually exists
