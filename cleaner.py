e6f1f0deef0457fa535e24c43c1b96988df14619
Natconly <79179892+Natconly@users.noreply.github.com>
2024-01-04 18:37:18 -0600

    Create the logic structure for how to check files
    This is not going to be a function code, but is to be the rough outline to fill in as I go.

--- cleaner.py
+++ cleaner.py
@@ -8,13 +8,33 @@ downloads_list = os.listdir(path)
 
 # Loop through each file and determine a "type", ie photo, pdf, misc
 for file in downloads_list:
+    try:                #if else structure to move files
+        if file == photos:
+            shutil(source path, /Users/nathans/Documents/Downloads_Cleanup/photos/)
+        elif file == printer:
+            shutil(source path, /Users/nathans/Documents/Downloads_Cleanup/printer/)
+        elif file == pdf:
+            shutil(source path, /Users/nathans/Documents/Downloads_Cleanup/pdf/)
+        elif file == installers:
+            shutil(source path, /Users/nathans/Documents/Downloads_Cleanup/installers/)
+        elif file == code:
+            shutil(source path, /Users/nathans/Documents/Downloads_Cleanup/code/)
+        elif file == data:
+            shutil(source path, /Users/nathans/Documents/Downloads_Cleanup/data/)
+    except:
+        shutil(source path, /Users/nathans/Documents/Downloads_Cleanup/data/)
     # if os.path.isfile(file):
     #     print(file)
-    break
+  
 
 
 #Once identified, move file
-
+photos = [".png", ".jpg", ".jpeg", ".heic"]
+printer = [".stl", "obj.", ".3mf"]
+pdf = [".pdf"]
+installers = [".dmg", ".iso"]
+code = [".py", ".mg"]
+data = [".exe", ".csv"]
 
 
 
