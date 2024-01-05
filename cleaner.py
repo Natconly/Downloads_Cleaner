49eaa8eb71371668d961521069604cbdc310b8ec
Natconly <79179892+Natconly@users.noreply.github.com>
2024-01-04 19:06:50 -0600

    Test Move function and file paths, update IF's

--- cleaner.py
+++ cleaner.py
@@ -4,25 +4,29 @@ import os, shutil
 # Need to have the code access the downloads folder
 path = os.chdir('../Downloads')
 downloads_list = os.listdir(path)
+
 # print(downloads_list)
+
+#test moving a documnet
+# shutil.move("/Users/nathans/Downloads/katho.jpg", "/Users/nathans/Documents/Downloads_Cleanup/photos/")
 
 # Loop through each file and determine a "type", ie photo, pdf, misc
 for file in downloads_list:
     try:                #if else structure to move files
         if file == photos:
-            shutil(source path, /Users/nathans/Documents/Downloads_Cleanup/photos/)
+            shutil.move(path + file, "/Users/nathans/Documents/Downloads_Cleanup/photos/" + file)
         elif file == printer:
-            shutil(source path, /Users/nathans/Documents/Downloads_Cleanup/printer/)
+            shutil.move(path + file, "/Users/nathans/Documents/Downloads_Cleanup/printer/" + file)
         elif file == pdf:
-            shutil(source path, /Users/nathans/Documents/Downloads_Cleanup/pdf/)
+            shutil.move(path + file, "/Users/nathans/Documents/Downloads_Cleanup/pdf/" + file)
         elif file == installers:
-            shutil(source path, /Users/nathans/Documents/Downloads_Cleanup/installers/)
+            shutil.move(path + file, "/Users/nathans/Documents/Downloads_Cleanup/installers/" + file)
         elif file == code:
-            shutil(source path, /Users/nathans/Documents/Downloads_Cleanup/code/)
+            shutil.move(path + file, "/Users/nathans/Documents/Downloads_Cleanup/code/" + file)
         elif file == data:
-            shutil(source path, /Users/nathans/Documents/Downloads_Cleanup/data/)
+            shutil.move(path + file, "/Users/nathans/Documents/Downloads_Cleanup/data/" + file)
     except:
-        shutil(source path, /Users/nathans/Documents/Downloads_Cleanup/data/)
+        shutil.move(path + file, "/Users/nathans/Documents/Downloads_Cleanup/other/" + file)
     # if os.path.isfile(file):
     #     print(file)
   
