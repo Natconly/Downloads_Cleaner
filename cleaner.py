932a2955b10534d9f386440268450084a0e26956
Natconly <79179892+Natconly@users.noreply.github.com>
2024-01-28 15:30:23 -0600

    Add a counter for each file type, display totals

--- cleaner.py
+++ cleaner.py
@@ -8,13 +8,19 @@ start = time.time()
 path = "/Users/nathans/Downloads/"
 downloads_list = os.listdir(path)
 
-#list of file types and destination folders
+#lists of file types and counters
 photos = [".png", ".jpg", ".jpeg", ".heic"]
+photo_count = 0
 printer = [".stl", "obj.", ".3mf"]
+printer_count = 0
 pdf = [".pdf"]
+pdf_count = 0
 installers = [".dmg", ".iso", ".zip"]
+installer_count = 0
 code = [".py", ".mg"]
+code_count = 0
 data = [".exe", ".csv",".pptx"]
+data_count = 0
 
 #test moving a documnet
 # shutil.move("/Users/nathans/Downloads/katho.jpg", "/Users/nathans/Documents/Downloads_Cleanup/photos/")
@@ -31,21 +37,27 @@ for file in downloads_list:
                    #if else structure to move files
     if file_type in photos:
         shutil.move(path + file, "/Users/nathans/Documents/Downloads_Cleanup/photos/" + file)
+        photo_count += 1
     elif file_type in printer:
         shutil.move(path + file, "/Users/nathans/Documents/Downloads_Cleanup/printer/" + file)
+        printer_count += 1
     elif file_type in pdf:
         shutil.move(path + file, "/Users/nathans/Documents/Downloads_Cleanup/pdf/" + file)
+        pdf_count += 1
     elif file_type in installers:
         shutil.move(path + file, "/Users/nathans/Documents/Downloads_Cleanup/installers/" + file)
+        installer_count += 1
     elif file_type in code:
         shutil.move(path + file, "/Users/nathans/Documents/Downloads_Cleanup/code/" + file)
+        code_count += 1
     elif file_type in data:
         shutil.move(path + file, "/Users/nathans/Documents/Downloads_Cleanup/data/" + file)
+        data_count += 1
     else:
         shutil.move(path + file, "/Users/nathans/Documents/Downloads_Cleanup/other/" + file)
 
 end = time.time()
-print("\nFinished! All files successfully moved.\nExecution time of the program is", end - start, "\n")
+print("\nFinished! All files successfully moved.\nExecution time of the program is", end - start, "\n",photo_count," Photo files moved\n", printer_count," Print files moved\n", pdf_count," PDF files moved\n", installer_count," Installation files moved\n", code_count," Code files moved\n", data_count," Data files moved\n")
 
 """ *******  Notes *******
 os.path.exists() - verifys that a file/directory location actually exists
